b507141d8144d8b02c796622ce66ba970ed3dbe2
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-03 22:12:27 +0600

    Home almost done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,32 +2,48 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex items-center m-10">
-      <div className="w-[800px] ">
-        <h1 className="text-5xl font-bold">
-          {" "}
-          <span className="text-sky-500">
-            YOUR NEXT PHONE<br></br>{" "}
-          </span>{" "}
-          WILL BE THE BEST PHONE!!!{" "}
-        </h1>
-        <p className="my-3 ">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla
-          dignissimos sint quaerat eos cumque harum ducimus id sunt illum velit
-          mollitia sapiente autem repellendus dicta iusto quasi rerum veniam,
-          illo magni consectetur natus labore sed! Temporibus harum distinctio
-          praesentium excepturi.
-        </p>
+    <div>
+      <div className="flex items-center m-10">
+        <div className="w-[800px] ">
+          <h1 className="text-5xl font-bold">
+            {" "}
+            <span className="text-sky-500">
+              YOUR NEXT PHONE<br></br>{" "}
+            </span>{" "}
+            WILL BE THE BEST PHONE!!!{" "}
+          </h1>
+          <p className="my-3 ">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla
+            dignissimos sint quaerat eos cumque harum ducimus id sunt illum
+            velit mollitia sapiente autem repellendus dicta iusto quasi rerum
+            veniam, illo magni consectetur natus labore sed! Temporibus harum
+            distinctio praesentium excepturi.
+          </p>
 
-        <button className="bg-red-500 p-2 rounded">Live Demo</button>
+          <button className="bg-green-300 p-2 rounded font-bold">
+            Live Demo
+          </button>
+        </div>
+
+        <div className="w-[700px]">
+          <img
+            className=" h-[500px]"
+            src="https://img.freepik.com/free-psd/premium-mobile-phone-screen-mockup-template_53876-65749.jpg?t=st=1649000045~exp=1649000645~hmac=b3cb2255460996e33615e3920cd1c66a101fdb42fa55df986875036987bc4c87&w=740"
+            alt=""
+          />
+        </div>
       </div>
 
-      <div className="w-[700px]">
-        <img
-          className=" h-[500px]"
-          src="https://img.freepik.com/free-psd/premium-mobile-phone-screen-mockup-template_53876-65749.jpg?t=st=1649000045~exp=1649000645~hmac=b3cb2255460996e33615e3920cd1c66a101fdb42fa55df986875036987bc4c87&w=740"
-          alt=""
-        />
+      <div className="my-16">
+        <h1 className="text-4xl font-bold">Customer Reviews(3)</h1>
+
+        <div className="review-cards">
+          <div className="cards"></div>
+        </div>
+
+        <button className="p-2 rounded bg-green-300 font-bold">
+          See All Reviews
+        </button>
       </div>
     </div>
   );
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from "react";
 const Notfound = () => {
   return (
     <div className="text-4xl font-bold text-red-500 mt-32">
-      404! the page you searching fo is not found
+      404! the page you searching for is not found
     </div>
   );
 };
