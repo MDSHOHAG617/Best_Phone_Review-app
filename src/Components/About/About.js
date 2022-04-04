7a0c3902412651dd76ea5d30b24dac4f63ee8759
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-05 00:26:33 +0600

    About done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,6 +4,16 @@ const About = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold my-5">Welcome to About</h1>
+      <div className="">
+        <h1 className="my-10  text-3xl text-red-500">
+          Single Manuser Abar Kiser About vai :D
+        </h1>
+        <img
+          className="w-[300px] m-auto"
+          src="https://i.pinimg.com/originals/61/5d/2b/615d2b6a7093f7d0fb8aebdfa91967cf.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
